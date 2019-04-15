e7fe6040ab8b622fb3ba896afc05dbffad4466cc
Charlie.Cao <964870916@qq.com>
2019-04-15 16:47:49 +0800

    Define policies

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -25,6 +25,33 @@ class AuthServiceProvider extends ServiceProvider
     {
         $this->registerPolicies();
 
-        //
+        $this->registerPostPolicies();
+    }
+    
+    /**
+     * 在认证服务者中检查用户权限
+     *
+     * @return void
+     */
+    public function registerPostPolicies()
+    {
+        //为Gate定义create-post 的检查方法, 当这个被调用的时候
+        // 就走后面的闭包,然后这个方法在相应的动作中调用.就可以起到检查当前用户是否有权访问的作用.
+        Gate::define('create-post', function ($user) {
+            //检查当前用户的角色有没有 create-post的权限.
+            return $user->hasAccess('create-post');
+        });
+        Gate::define('update-post', function ($user, Post $post) {
+            return $user->hasAccess('update-post') or $user->id == $post->user_id;
+        });
+        Gate::define('publish-post', function ($user, Post $post) {
+            return $user->hasAccess('publish-post') or $user->id == $post->user_id;;
+        });
+        Gate::define('delete-post', function ($user, Post $post) {
+            return $user->hasAccess('delete-post') or $user->id == $post->user_id;
+        });
+        Gate::define('see-all-drafts', function ($user) {
+            return $user->inRole('editor');
+        });
     }
 }
